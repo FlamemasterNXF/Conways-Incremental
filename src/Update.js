389b9233574957528680e724303e8402be4e7c3a
FlamemasterNXF <flamemasternxf@gmail.com>
2022-01-23 09:53:21 -0500

    Remove Essence :derpsmith:

--- src/Update.js
+++ src/Update.js
@@ -2,5 +2,5 @@ const cellsAliveText = document.getElementById('cellsAlive')
 const essenceText = document.getElementById('essence')
 function updateHTML(){
     cellsAliveText.innerText = `There are ${formatWhole(data.aliveCells)} Cells currently alive`
-    essenceText.innerText = `There is ${format(data.essence)} Essence, your Cells generate ${format(essenceGain)}/s`
+    //essenceText.innerText = `There is ${format(data.essence)} Essence, your Cells generate ${format(essenceGain)}/s`
 }
